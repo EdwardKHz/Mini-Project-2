afa683db42a683e979961402e68b7e0131e5ead0
Edward Khazzoum <edwardkhazzoum@gmail.com>
2025-03-21 22:24:43 +0200

    Added Employee's model and controller classes

--- src/main/java/com/workshop1/miniproject2/models/Employee.java
+++ src/main/java/com/workshop1/miniproject2/models/Employee.java
@@ -1,4 +1,51 @@
 package com.workshop1.miniproject2.models;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Employee {
+    private final SimpleStringProperty firstName, lastName;
+    private final SimpleIntegerProperty age;
+    private final SimpleStringProperty address;
+    private final SimpleDoubleProperty salary;
+
+    public Employee(String firstName, String lastName, int age, String address, double salary) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.age = new SimpleIntegerProperty(age);
+        this.address = new SimpleStringProperty(address);
+        this.salary = new SimpleDoubleProperty(salary);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+    public String getLastName() {
+        return lastName.get();
+    }
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+    public int getAge() {
+        return age.get();
+    }
+    public void setAge(int age) {
+        this.age.set(age);
+    }
+    public String getAddress() {
+        return address.get();
+    }
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+    public double getSalary() {
+        return salary.get();
+    }
+    public void setSalary(double salary) {
+        this.salary.set(salary);
+    }
 }
--- src/main/java/com/workshop1/miniproject2/models/EmployeeStore.java
+++ src/main/java/com/workshop1/miniproject2/models/EmployeeStore.java
@@ -1,4 +1,39 @@
 package com.workshop1.miniproject2.models;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class EmployeeStore {
+    private final ObservableList<Employee> employees = FXCollections.observableArrayList();
+
+    public EmployeeStore() {
+        this.employees.addAll(
+                new Employee("John", "Smith", 24, "Beirut", 2300),
+                new Employee("Perla", "Khazzoum", 19, "Antelias",4300)
+        );
+    }
+
+    public ObservableList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        if (employee != null) {
+            employees.add(employee);
+        }
+    }
+    public void deleteEmployee(Employee employee) {
+        employees.remove(employee);
+    }
+
+    public void updateEmployee(Employee employee, String firstName, String lastName, int age, String address, double salary) {
+        if (employee != null) {
+            employee.setFirstName(firstName);
+            employee.setLastName(lastName);
+            employee.setAge(age);
+            employee.setAddress(address);
+            employee.setSalary(salary);
+        }
+    }
+
 }
