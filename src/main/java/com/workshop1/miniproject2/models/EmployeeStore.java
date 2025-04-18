1135957d387d0d20a69d107eb73d918aee11137b
Edward Khazzoum <edwardkhazzoum@gmail.com>
2025-04-18 14:48:16 +0300

    Fixed issues with EmployeeStore

--- src/main/java/com/workshop1/miniproject2/models/EmployeeStore.java
+++ src/main/java/com/workshop1/miniproject2/models/EmployeeStore.java
@@ -53,7 +53,7 @@ public class EmployeeStore {
                 statement.setInt(3, employee.getAge());
                 statement.setDouble(4, employee.getSalary());
                 statement.executeUpdate();
-                refreshTable();
+                employees.add(employee);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -72,7 +72,7 @@ public class EmployeeStore {
             statement.setInt(3, employee.getAge());
             statement.setDouble(4, employee.getSalary());
             statement.executeUpdate();
-            refreshTable();
+            employees.remove(employee);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -86,46 +86,23 @@ public class EmployeeStore {
             Connection conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
 
              PreparedStatement statement = conn.prepareStatement(query);
-
-           statement.setString(1, employee.getFirstName());
-           statement.setString(2, employee.getLastName());
-           statement.setInt(3, employee.getAge());
-           statement.setDouble(4, employee.getSalary());
-           statement.setString(5, firstName);
-           statement.setString(6, lastName);
-           statement.setInt(7, age);
-           statement.setDouble(8, salary);
+           statement.setString(1, firstName);
+           statement.setString(2, lastName);
+           statement.setInt(3, age);
+           statement.setDouble(4, salary);
+            statement.setString(5, employee.getFirstName());
+            statement.setString(6, employee.getLastName());
+            statement.setInt(7, employee.getAge());
+            statement.setDouble(8, employee.getSalary());
+            employee.setFirstName(firstName);
+            employee.setLastName(lastName);
+            employee.setAge(age);
+            employee.setSalary(salary);
            statement.executeUpdate();
-           refreshTable();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private void refreshTable() {
-        String query = "SELECT * FROM employees";
-        ObservableList<Employee> refresnedList = FXCollections.observableArrayList();
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(query);
-
-            while (rs.next()) {
-                Employee employee = new Employee(
-                        rs.getString("firstName"),
-                        rs.getString("lastName"),
-                        rs.getInt("age"),
-                        rs.getDouble("salary")
-                );
-                refresnedList.add(employee);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        employees.setAll(refresnedList);
-    }
-
-
 
 }
