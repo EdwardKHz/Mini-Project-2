3612ced08d9ce4c2b7d20ba4263c1baab793a252
Edward Khazzoum <edwardkhazzoum@gmail.com>
2025-05-06 14:41:09 +0300

    login controller

--- src/main/java/com/workshop1/miniproject2/models/Employee.java
+++ src/main/java/com/workshop1/miniproject2/models/Employee.java
@@ -5,9 +5,19 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Employee {
+    private int id;
     private final SimpleStringProperty firstName, lastName;
     private final SimpleIntegerProperty age;
     private final SimpleDoubleProperty salary;
+
+
+    public Employee(int id,String firstName, String lastName, int age, double salary) {
+        this.id = id;
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.age = new SimpleIntegerProperty(age);
+        this.salary = new SimpleDoubleProperty(salary);
+    }
 
     public Employee(String firstName, String lastName, int age, double salary) {
         this.firstName = new SimpleStringProperty(firstName);
@@ -16,6 +26,12 @@ public class Employee {
         this.salary = new SimpleDoubleProperty(salary);
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getFirstName() {
         return firstName.get();
     }
